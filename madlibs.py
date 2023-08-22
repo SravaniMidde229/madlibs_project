8cb58c0a7f547ae127951a94667bb38b3eb1a8cc
SravaniMidde229 <128965631+SravaniMidde229@users.noreply.github.com>
2023-08-22 19:40:24 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+blank1 = input("blank1: ")
+blank2 = input("blank2: ")
+blank3 = input("blank3: ")
+blank4 = input("blank4: ")
+
+madlibs = f"Computer programming is so {blank1}! It makes me so excited all the time because \
+I love to {blank2}. Stay hydrated and {blank3} like you are {blank4}!"
+
+print(madlibs)
